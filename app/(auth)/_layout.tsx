a1f98a18429d521330d3e8c1e3b97006012ba0fd
Jospay <joshuapayumo2001@gmail.com>
2026-04-08 15:54:39 +0800

    feat:  auth page layout page

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -12,28 +12,63 @@ export default function RootLayout() {
   useEffect(() => {
     const hideNavBar = async () => {
       if (Platform.OS === "android") {
-        await NavigationBar.setVisibilityAsync("hidden");
-        await NavigationBar.setBehaviorAsync("inset-touch");
+        try {
+          // Changed to "as any" to force the red line to disappear
+          await NavigationBar.setBehaviorAsync("sticky-immersive" as any);
+          await NavigationBar.setVisibilityAsync("hidden");
+        } catch (e) {
+          console.log("NavigationBar error:", e);
+        }
       }
     };
+
     hideNavBar();
   }, []);
 
   return (
     <QueryClientProvider client={queryClient}>
-      <StatusBar style="light" />
+      <StatusBar hidden={true} />
+
       <View className="flex-1 bg-slate-50">
         <KeyboardAvoidingView
           behavior={Platform.OS === "ios" ? "padding" : "height"}
           className="flex-1"
+          keyboardVerticalOffset={Platform.OS === "android" ? 40 : 0}
         >
           <Stack
             screenOptions={{
               headerShown: false,
-              animation: "none",
+              animation: "fade",
               contentStyle: { backgroundColor: "#f8fafc" },
             }}
-          />
+          >
+            <Stack.Screen name="login" options={{ animation: "fade" }} />
+            <Stack.Screen
+              name="register"
+              options={{ animation: "slide_from_right" }}
+            />
+
+            <Stack.Screen name="otpSend" options={{ animation: "fade" }} />
+            <Stack.Screen
+              name="otpVerification"
+              options={{ animation: "fade" }}
+            />
+
+            <Stack.Screen
+              name="successVerification"
+              options={{ animation: "fade" }}
+            />
+
+            <Stack.Screen
+              name="createPassword"
+              options={{ animation: "fade" }}
+            />
+
+            <Stack.Screen
+              name="congratulations"
+              options={{ animation: "fade" }}
+            />
+          </Stack>
         </KeyboardAvoidingView>
       </View>
     </QueryClientProvider>
