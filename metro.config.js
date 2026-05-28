a59647c65a06eced0207e24205e01eddb62b8157
Jospay <joshuapayumo2001@gmail.com>
2026-05-28 17:00:33 +0800

    chore: update dependencies in package.json
    - Added expo-linear-gradient version ~55.0.14
    - Updated nativewind from ^4.2.3 to ^4.2.4
    - Added react-native-image-zoom-viewer version ^3.0.1
    - Added react-native-render-html version ^6.3.4
    - Updated tailwindcss to version ^3.4.17

--- metro.config.js
+++ metro.config.js
@@ -5,5 +5,4 @@ const config = getDefaultConfig(__dirname);
 
 module.exports = withNativeWind(config, {
   input: "./global.css",
-  inlineRem: 16, // This matches the web standard (1rem = 16px)
 });
