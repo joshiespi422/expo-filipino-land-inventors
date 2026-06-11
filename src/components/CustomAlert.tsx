cde961b6b30adeb51822c03fe76ebca896d2dc59
Jospay <joshuapayumo2001@gmail.com>
2026-06-11 13:35:05 +0800

    feat(alert): refactor CustomAlert component for improved structure and styling

--- src/components/CustomAlert.tsx
+++ src/components/CustomAlert.tsx
@@ -1,6 +1,13 @@
 import * as NavigationBar from "expo-navigation-bar";
 import React, { useEffect } from "react";
-import { Modal, Platform, Text, TouchableOpacity, View } from "react-native";
+import {
+  Modal,
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 interface CustomAlertProps {
   visible: boolean;
@@ -8,7 +15,7 @@ interface CustomAlertProps {
   message: string;
   onClose: () => void;
   onConfirm?: () => void;
-  confirmText?: string; // Added optional prop for flexible action button text
+  confirmText?: string;
 }
 
 export const CustomAlert = ({
@@ -17,7 +24,7 @@ export const CustomAlert = ({
   message,
   onClose,
   onConfirm,
-  confirmText = "Logout", // Defaults to "Logout" so your existing logout flows don't break
+  confirmText = "Logout",
 }: CustomAlertProps) => {
   useEffect(() => {
     if (visible && Platform.OS === "android") {
@@ -36,16 +43,9 @@ export const CustomAlert = ({
       statusBarTranslucent={true}
       onRequestClose={onClose}
     >
-      <View
-        className="flex-1 justify-center items-center bg-black/60 px-8"
-        style={{
-          width: "100%",
-          height: "100%",
-          position: "absolute",
-          top: 0,
-          left: 0,
-        }}
-      >
+      {/* OVERLAY */}
+      <View style={styles.modalOverlay} className="p-6">
+        {/* MODAL BOX */}
         <View className="bg-white w-full max-w-[400px] rounded-[35px] p-6 shadow-2xl elevation-10">
           <Text className="text-primary text-2xl font-bold text-center mb-3">
             {title}
@@ -55,7 +55,6 @@ export const CustomAlert = ({
             {message}
           </Text>
 
-          {/* Logic to switch between 1 button or 2 buttons */}
           {onConfirm ? (
             <View className="flex-row gap-x-3">
               <TouchableOpacity
@@ -66,6 +65,7 @@ export const CustomAlert = ({
                   Cancel
                 </Text>
               </TouchableOpacity>
+
               <TouchableOpacity
                 onPress={onConfirm}
                 className="flex-1 p-4 bg-primary rounded-2xl"
@@ -91,3 +91,12 @@ export const CustomAlert = ({
     </Modal>
   );
 };
+
+const styles = StyleSheet.create({
+  modalOverlay: {
+    flex: 1,
+    backgroundColor: "rgba(0, 0, 0, 0.4)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
